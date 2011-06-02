6bd6286cf5d83621b3c53458140fb815081a340d
Sid Carter <sidcarter@gmail.com>
2011-06-02 19:45:01 +1200

    updates

--- main.js
+++ main.js
@@ -1,36 +1,17 @@
 var http = require('http'),
-	sys = require('sys');
+	sys = require('sys'),
+	slave = require('./functions');
 
-var location = process.env.PWD;
-
-var server = http.createServer(function(request,response){
-	response.writeHead(200,{
-		'Content-type': 'text/html'
-	});
-
-var options = {
-	host: 'localhost',
-	port: 5984,
-	path: '/'
-}
-
-	http.get(options, function(res) {
-		res.on('data', function(chunk) {
-			response.write("Couchdb Info" + chunk);
-		});
-	}).on('error', function(errmessage) {
-		console.log('Got ' + errmessage);
-	})
-
-	response.write('Hello You!<br>');
-	response.end('Running ' + process.title + ' version: ' + process.version);
+process.on('TypeError', function(e) {
+	console.log(e);
 })
+
+var server = http.createServer();
+
+server.on('request', slave.respond);
+
+server.on('connection', slave.log);
 
 server.listen(4242);
 
 console.log('Server listening on http://127.0.0.1:4242');
-
-server.on('connection', function(stream) {
-	console.log('someone connected' + stream.data);
-})
-
