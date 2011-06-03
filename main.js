a0d6e8e4194163a52a6b40b4a5901ab381e6ce7e
Sid Carter <sidcarter@gmail.com>
2011-06-04 00:53:44 +1200

    Cleanup whitespaces

--- main.js
+++ main.js
@@ -13,20 +13,3 @@ server.on('request', slave.respond);
 server.listen(4242);
 
 console.log('Server listening on http://127.0.0.1:4242');
-
-/* 
-var couchDbOptions = {
-	host: 'google.com',
-	port: 80,
-	path: '/'
-}
-
-var couchDbRequest = http.get(couchDbOptions, function(response, err) {
-	if (err) {
-		throw err;
-	};
-	response.on('data', function(chunk) {
-		console.log("Body: " + chunk);
-	})
-});
-*/
