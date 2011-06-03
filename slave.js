078c2c16d7b6560ebd02d9239fd80dbb8a5017c0
Sid Carter <sidcarter@gmail.com>
2011-06-03 21:06:23 +1200

    The skeleton forms

--- slave.js
+++ slave.js
@@ -5,33 +5,56 @@ var url = require('url'),
 
 
 function respond(request,response){
-
-	console.log('Connection from: ' + request.headers['x-real-ip'] + ' for url: ' + request.url);
-
-	fullUrl = url.parse(request.url);
-	
-	NotFound(request, response);
-	
-	route(fullUrl);
-}
-
-function route(url){
-	console.log('User wants to go to : ' + url.pathname);
-}
-
-function NotFound(request,response){
 	
 	var remoteAddress = request.headers['x-real-ip'] || request.socket.remoteAddress;
+
+	console.log('Connection from: ' + remoteAddress + ' for url: ' + request.url);
+
+	fullUrl = url.parse(request.url);
+
+	route(fullUrl, response);
+}
+
+function route(url, response){
+
+	var pathname = url.pathname.split('/');
+	var path = pathname[1];
+	console.log('User wants to go to : ' + path);
+	
+	switch(path) {
+		case '' : 
+			response.writeHead(200,{
+				'Content-type': 'text/html'
+			});
+			response.end('In index');
+			break;
+	
+		case 'testing': 
+			response.writeHead(200,{
+				'Content-type': 'text/html'
+			});
+			response.end('In testing now');
+			break;
+	
+		default: 
+			NotFound(url.pathname, response);
+			break;
+	}
+}
+
+function NotFound(pathname,response){
 	
 	response.writeHead(404,{
 		'Content-type': 'text/html'
 	});
 	
 	response.write('Hello You!<br/>');
-	response.write('You are connecting from : ' + remoteAddress + '<br/>');
-	response.write('to access the URL: ' + request.headers.host + request.url + '<br/>');
-	response.write('My current dir: ' + __dirname + '<br/>');
+	response.write('You want the URL: ' + pathname + ', which incidentally does not exist yet.<br/>');
 	response.end('Running ' + process.title + ' version: ' + process.version);
+}
+
+function render(pathname) {
+	
 }
 
 exports.respond = respond;
