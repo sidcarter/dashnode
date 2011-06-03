d1a39433d6e1d3ab1aa183123cf05e453b57fcc8
Sid Carter <sidcarter@gmail.com>
2011-06-03 18:33:26 +1200

    The frankenstein slowly builds

--- slave.js
+++ slave.js
@@ -1,24 +1,37 @@
-var url = require('url');
-var haml = require('haml');
-var sass = require('sass');
+var url = require('url'),
+	haml = require('haml'),
+	sass = require('sass'),
+	fs = require('fs');
 
 
 function respond(request,response){
-	response.writeHead(200,{
+
+	console.log('Connection from: ' + request.headers['x-real-ip'] + ' for url: ' + request.url);
+
+	fullUrl = url.parse(request.url);
+	
+	NotFound(request, response);
+	
+	route(fullUrl);
+}
+
+function route(url){
+	console.log('User wants to go to : ' + url.pathname);
+}
+
+function NotFound(request,response){
+	
+	var remoteAddress = request.headers['x-real-ip'] || request.socket.remoteAddress;
+	
+	response.writeHead(404,{
 		'Content-type': 'text/html'
 	});
-
-	route(request);
-
+	
 	response.write('Hello You!<br/>');
-	response.write('You are connecting from : ' + request.socket.remoteAddress + '<br/>');
+	response.write('You are connecting from : ' + remoteAddress + '<br/>');
 	response.write('to access the URL: ' + request.headers.host + request.url + '<br/>');
 	response.write('My current dir: ' + __dirname + '<br/>');
 	response.end('Running ' + process.title + ' version: ' + process.version);
-}
-
-function route(request){
-	console.log('Connection from: ' + request.headers['x-real-ip'] + ' for url: ' + request.url);
 }
 
 exports.respond = respond;
