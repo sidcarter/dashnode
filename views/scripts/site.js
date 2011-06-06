a70418310c6105e62333817a48616e93dec9e3dd
Sid Carter <sidcarter@gmail.com>
2011-06-06 23:01:35 +1200

    site.js fixes

--- views/scripts/site.js
+++ views/scripts/site.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 	var $h1head = $('span.head');
-	var $h1welcome = $('<span class=welcome>');
-	$h1welcome.html('Welcome to ');
-	$h1welcome.insertBefore($h1head).fadeOut(2000, function(){
+	var $h1welcome = $('span.welcome');
+	$h1welcome.fadeOut(2000, function(){
 		$('.footer').animate({
 				opacity: 0.5},
 				2000);
