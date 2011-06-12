d2e2a931a846e8b1c6a06f5827314f8ed98bf492
Sid Carter <sidcarter@gmail.com>
2011-06-12 18:45:29 +1200

    Adding a bit of content

--- views/scripts/site.js
+++ views/scripts/site.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 	var $h1head = $('span.head');
-	var $h1welcome = $('span.welcome');
-	$h1welcome.fadeOut(2000, function(){
-		$('.footer').animate({
-				opacity: 0.5},
-				2000);
-		$h1head.show(2000);
-	});
+	$('.footer').animate({
+		opacity: 0.5},
+		2000
+	);
 });
